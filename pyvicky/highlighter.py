d9b30a85be6cffd2b8500e16adb30287a5c99667
vereline <vstanko1998@gmail.com>
2018-04-06 01:26:57 +0300

    Define window features and properties

---
+++ pyvicky/highlighter.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import logging
+import configparser
+
+
+class BaseHighlighter:
+    pass
+
+
+class PythonHighlighter(BaseHighlighter):
+    pass
+
+
+class CppHighlighter(BaseHighlighter):
+    pass
---
+++ pyvicky/numberbar.py
@@ -0,0 +1,1 @@
+# bar with numbers for counting rows
